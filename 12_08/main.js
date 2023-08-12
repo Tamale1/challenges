a7be6c5b3dc440f5030d10d8b892ef69a8208549
Tamale1 <yusufnkoyoyo@gmail.com>
2023-08-12 17:22:08 +0300

    revised

--- 12_08/main.js
+++ 12_08/main.js
@@ -1,18 +1,21 @@
 function positiveSum(arr) {
-    let total =0
-    for(let i =0; i< arr.length; i++){
-    if (arr[i]>0) {
-return total += arr[i]
-        
+    var total = 0;    
+    for (i = 0; i < arr.length; i++) {    
+      if (arr[i] > 0) {                   
+        total += arr[i];                  
+      }
     }
-    }
-
- return  (console.log(total ))
-  
-}
+    return total;                         
+  }
 
 // You get an array of numbers, return the sum of all of the positives ones.
 
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 // Note: if there is nothing to sum, the sum is default to 0.
+
+
+// how to pass a string to an integer 
+// const stringToNumber = function(str){
+//     return parseInt(str)
+//     }
