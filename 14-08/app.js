f081e770d4a98b94cfc2e7714e3e123fc6fc82ba
Tamale1 <yusufnkoyoyo@gmail.com>
2023-08-14 15:46:07 +0300

    more arr

--- 14-08/app.js
+++ 14-08/app.js
@@ -7,3 +7,14 @@ class Kata {
      return vol
     }
   }
+
+
+  //multiplication of array elements
+
+  function grow(x){
+    let product = 1
+  for(let i=0; i < x.length; i++){
+    product*= x[i]
+  }
+    return product
+  }
