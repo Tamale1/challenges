12bd530e8ce1403ade48ccce877b0f8db18ab513
Tamale1 <yusufnkoyoyo@gmail.com>
2023-12-02 13:14:40 +0300

    more challenges

--- strings/02/12/strings.js
+++ strings/02/12/strings.js
@@ -12,4 +12,28 @@ function getCount(str) {
 
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
-// The input string will only consist of lower case letters and/or spaces.
+// The input string will only consist of lower case letters and/or spaces.\
+
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+
+
+function removeExclamationMarks(s) {
+    let newString = []
+    s.split('').forEach(el=>{
+      if(el !=='!'){
+       newString.push(el)
+      }
+    })
+   return newString.join('')
+
+   }
+//    Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
